61ecb925672231dc5479cbdff49c62a6732ada00
greedypanda0 <dream.lucky.ok@gmail.com>
2025-07-24 18:22:18 +0530

    fixed some ui

--- src/features/home/components/misson.jsx
+++ src/features/home/components/misson.jsx
@@ -6,12 +6,12 @@ export function Mission() {
   
   return (
     <div className="w-full px-4 py-8 flex flex-col md:flex-row">
-      <div className="md:w-1/3 w-full flex items-center justify-center bg-primary/40 rounded-l-xl">
+      <div className="md:w-1/3 w-full flex items-center justify-center md:bg-primary/40 rounded-l-xl">
         <h2 className="text-2xl font-semibold text-foreground text-center md:text-left">
           {t("title")}
         </h2>
       </div>
-      <div className="md:w-2/3 w-full flex items-center border-2 border-primary/40 p-6 rounded-r-xl">
+      <div className="md:w-2/3 w-full flex items-center md:border-2 border-primary/40 py-6 md:p-6 rounded-r-xl">
         <p className="text-base text-primary-foreground leading-relaxed">
           {t("description")}
         </p>
